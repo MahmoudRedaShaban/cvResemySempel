4463c5b25d58fbb01bc05b5969a3f99aeee04e0b
Mahmoud Reda <mrmrmr033@gmail.com>
2021-04-15 17:35:07 +0200

    Init Project Rsemy Using Lumen Api - and init all migration and  models and controller

---
+++ app/Http/Controllers/ExperiencesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Experiences;
+use Illuminate\Http\Request;
+
+class ExperiencesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Experiences  $experiences
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Experiences $experiences)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Experiences  $experiences
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Experiences $experiences)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Experiences  $experiences
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Experiences $experiences)
+    {
+        //
+    }
+}
